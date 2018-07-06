7aef48b32588b7d6a01f2bae45c8c5ab00e16830
alxalx31 <aroy@ausy-group.com>
2018-07-06 12:43:38 +0200

    Commit menuAdmin + Creer Rencontre

--- ProjetWinBet/src/com/winbet/entities/Equipe.java
+++ ProjetWinBet/src/com/winbet/entities/Equipe.java
@@ -1,5 +1,50 @@
 package com.winbet.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Equipe {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	@OneToOne
+	private Sport sport;
+
+	public Equipe() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Sport getSport() {
+		return sport;
+	}
+
+	public void setSport(Sport sport) {
+		this.sport = sport;
+	}
+
+	public String toString() {
+		return "Equipe [id=" + id + ", nom=" + nom + ", sport=" + sport + "]";
+	}
 
 }
