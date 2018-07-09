d90151fa035aa3d12ebee5e5b70d94c64b14bcf2
alxalx31 <aroy@ausy-group.com>
2018-07-09 15:37:09 +0200

    correction client

--- ProjetWinBet/src/com/winbet/entities/Client.java
+++ ProjetWinBet/src/com/winbet/entities/Client.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
