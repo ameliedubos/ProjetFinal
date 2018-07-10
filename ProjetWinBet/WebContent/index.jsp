0ca1eb04c7c7f00453902fe2ffbd42a25548e720
Paul Binagwaho <pbinagwaho@ausy.fr>
2018-07-10 17:07:21 +0200

    Welcome controller updated

--- ProjetWinBet/WebContent/index.jsp
+++ ProjetWinBet/WebContent/index.jsp
@@ -1,2 +1,2 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<c:redirect url="http://localhost:8080/ProjetWinBet/client/goToAccueil" />
+<c:redirect url="http://localhost:8080/ProjetWinBet/welcome/goToAccueil" />
--- ProjetWinBet/src/com/winbet/controllers/WelcomeController.java
+++ ProjetWinBet/src/com/winbet/controllers/WelcomeController.java
@@ -11,10 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.winbet.dao.IAdminJpaRepository;
 import com.winbet.dao.IEquipeJpaRepository;
 import com.winbet.dao.ISportJpaRepository;
-import com.winbet.entities.Admin;
 import com.winbet.entities.Authentification;
-import com.winbet.entities.ERole;
-import com.winbet.entities.Equipe;
 import com.winbet.entities.Sport;
 
 @Controller
@@ -30,7 +27,7 @@ public class WelcomeController {
 
     @RequestMapping("/goToAccueil")
     public String goToAccueil(Model model) {
-	createElementsInDataBase();
+	// createElementsInDataBase();
 	if (!AuthHelper.isAuthenticated()) {
 	    List<Sport> listeSports = sportRepo.findAll();
 	    model.addAttribute("listeSports", listeSports);
@@ -48,53 +45,62 @@ public class WelcomeController {
 	authentification.setMotDePasse(encodedPassword);
     }
 
-    private void createElementsInDataBase() {
-	Admin admin = new Admin();
-	Authentification auth = new Authentification();
-	auth.setEmail("root@root.fr");
-	auth.setMotDePasse("123");
-	auth.setRole(ERole.ROLE_ADMIN);
-	encodePassword(auth);
-	admin.setAuthentification(auth);
-	AdminRepo.save(admin);
-
-	Sport sports1 = new Sport();
-	sports1.setNom("Football");
-	Sport sports2 = new Sport();
-	sports2.setNom("Rugby");
-	Sport sports3 = new Sport();
-	sports3.setNom("Basketball");
-	Sport sports4 = new Sport();
-	sports4.setNom("Tennis");
-	Sport sports5 = new Sport();
-	sports5.setNom("Handball");
-	Sport sports6 = new Sport();
-	sports6.setNom("Volleyball");
-	sportRepo.save(sports1);
-	sportRepo.save(sports2);
-	sportRepo.save(sports3);
-	sportRepo.save(sports4);
-	sportRepo.save(sports5);
-	sportRepo.save(sports6);
-
-	Equipe equipe1 = new Equipe();
-	equipe1.setNom("PSG");
-	Equipe equipe2 = new Equipe();
-	equipe2.setNom("CHELSEA");
-	Equipe equipe3 = new Equipe();
-	equipe3.setNom("GOLDENSTATE");
-	Equipe equipe4 = new Equipe();
-	equipe4.setNom("CHICAGO BULLS");
-	Equipe equipe5 = new Equipe();
-	equipe5.setNom("TOULON");
-	Equipe equipe6 = new Equipe();
-	equipe6.setNom("CASTRE OLYMPIQUE");
-	equipeRepo.save(equipe1);
-	equipeRepo.save(equipe2);
-	equipeRepo.save(equipe3);
-	equipeRepo.save(equipe4);
-	equipeRepo.save(equipe5);
-	equipeRepo.save(equipe6);
-
-    }
+    // private void createElementsInDataBase() {
+    // Admin admin = new Admin();
+    // Authentification auth = new Authentification();
+    // auth.setEmail("root@root.fr");
+    // auth.setMotDePasse("123");
+    // auth.setRole(ERole.ROLE_ADMIN);
+    // encodePassword(auth);
+    // admin.setAuthentification(auth);
+    // AdminRepo.save(admin);
+    //
+    // Sport sports1 = new Sport();
+    // sports1.setNom("Football");
+    // Sport sports2 = new Sport();
+    // sports2.setNom("Rugby");
+    // Sport sports3 = new Sport();
+    // sports3.setNom("Basketball");
+    // Sport sports4 = new Sport();
+    // sports4.setNom("Tennis");
+    // Sport sports5 = new Sport();
+    // sports5.setNom("Handball");
+    // Sport sports6 = new Sport();
+    // sports6.setNom("Volleyball");
+    //
+    // Equipe equipe1 = new Equipe();
+    // equipe1.setNom("PSG");
+    // Equipe equipe2 = new Equipe();
+    // equipe2.setNom("CHELSEA");
+    // Equipe equipe3 = new Equipe();
+    // equipe3.setNom("GOLDENSTATE");
+    // Equipe equipe4 = new Equipe();
+    // equipe4.setNom("CHICAGO BULLS");
+    // Equipe equipe5 = new Equipe();
+    // equipe5.setNom("TOULON");
+    // Equipe equipe6 = new Equipe();
+    // equipe6.setNom("CASTRE OLYMPIQUE");
+    //
+    // sportRepo.save(sports1);
+    // sportRepo.save(sports2);
+    // sportRepo.save(sports3);
+    // sportRepo.save(sports4);
+    // sportRepo.save(sports5);
+    // sportRepo.save(sports6);
+    //
+    // equipe1.setSport(sports1);
+    // equipe2.setSport(sports1);
+    // equipe3.setSport(sports3);
+    // equipe4.setSport(sports3);
+    // equipe5.setSport(sports2);
+    // equipe6.setSport(sports2);
+    //
+    // equipeRepo.save(equipe1);
+    // equipeRepo.save(equipe2);
+    // equipeRepo.save(equipe3);
+    // equipeRepo.save(equipe4);
+    // equipeRepo.save(equipe5);
+    // equipeRepo.save(equipe6);
+    //
+    // }
 }
